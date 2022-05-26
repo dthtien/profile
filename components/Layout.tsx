73204e6d49f61fe61a7f9153851a5f90dddd0f05
dthtien <tien.dau@employmenthero.com>
2022-05-26 17:20:35 +0700

    chore(layout): correct indent

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,8 +6,8 @@ const Layout: NextPage<{ children: React.ReactNode; }> = ({ children }) => (
   <div className="relative bg-white overflow-hidden">
     <div className="max-w-7xl mx-auto">
       <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:w-full lg:pb-28 xl:pb-32">
-      <Nav />
-      { children }
+        <Nav />
+        { children }
       </div>
     </div>
   </div>
