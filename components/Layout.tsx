7e7406042b637c9a43d3d7cc3ae6f8a2b3bd9573
dthtien <tien.dau@employmenthero.com>
2022-05-26 16:59:18 +0700

    chore(app): setup layout

---
+++ components/Layout.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import React from 'react'
+import Nav from './Nav'
+
+const Layout: NextPage<{ children: React.ReactNode; }> = ({ children }) => (
+  <div className="relative bg-white overflow-hidden">
+    <div className="max-w-7xl mx-auto">
+      <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:w-full lg:pb-28 xl:pb-32">
+      <Nav />
+      { children }
+      </div>
+    </div>
+  </div>
+)
+
+export default Layout;
