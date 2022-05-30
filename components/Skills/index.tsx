d5a0a32fb8870a2be16a60770ac2b38008ed0ca2
dthtien <tien.dau@employmenthero.com>
2022-05-30 21:57:25 +0700

    chore(skills): update icon

--- components/Skills/index.tsx
+++ components/Skills/index.tsx
@@ -67,7 +67,7 @@ export default function Example() {
           <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
             <dt className="text-sm font-extrabold text-gray-500">Paradigms</dt>
             <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              Behavior-driven Development, Test-driven Development, Object-oriented Programming, Responsive Web Design, Model-View-Controlle
+              Behavior-driven Development, Test-driven Development, Object-oriented Programming, Responsive Web Design, Model-View-Controller
             </dd>
           </div>
         </dl>
