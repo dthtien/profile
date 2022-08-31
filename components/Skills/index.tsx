700e0dd19b2ccd4077784f903dacec2174b62df2
dthtien <tien.dau@employmenthero.com>
2022-08-31 19:17:55 +0700

    chore(skill): update

--- components/Skills/index.tsx
+++ components/Skills/index.tsx
@@ -105,7 +105,7 @@ const Skills = () => (
         <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
           <dt className="text-sm font-extrabold text-gray-500">Paradigms</dt>
           <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-            Behavior-driven Development, Test-driven Development,
+            Event-driven Development, Behavior-driven Development, Test-driven Development,
             Object-oriented Programming, Responsive Web Design,
             Model-View-Controller
           </dd>
