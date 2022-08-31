47d3164796278a2acc3144221245077d50e4818c
dthtien <tien.dau@employmenthero.com>
2022-08-31 16:46:34 +0700

    chore(app): correct margin

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -21,7 +21,7 @@ const Nav: FC = () => (
             </div>
           </div>
         </div>
-        <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
+        <div className="hidden md:block md:pr-4 md:space-x-8">
           {navigation.map((item) => (
             <a key={item.name} href={item.href} className="font-medium text-gray-500 hover:text-gray-900">
               {item.name}
